6ecf6ee24b76f9d6491396875d0f0f7186cd44e8
Matthew Rothenberg <mrothenberg@gmail.com>
2013-05-24 19:10:10 -0400

    move setup into its own file

---
+++ lib/config.rb
@@ -0,0 +1,12 @@
+# configure tweetstream instance
+TweetStream.configure do |config|
+  config.consumer_key       = ENV['CONSUMER_KEY']
+  config.consumer_secret    = ENV['CONSUMER_SECRET']
+  config.oauth_token        = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+  config.auth_method = :oauth
+end
+
+# db setup
+uri = URI.parse(ENV["BOXEN_REDIS_URL"] || ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- streamer.rb
+++ streamer.rb
@@ -3,20 +3,9 @@ require 'oj'
 require 'colored'
 require 'redis'
 require 'uri'
+
+require_relative 'lib/config'
 require_relative 'lib/emoji'
-
-# configure tweetstream instance
-TweetStream.configure do |config|
-  config.consumer_key       = ENV['CONSUMER_KEY']
-  config.consumer_secret    = ENV['CONSUMER_SECRET']
-  config.oauth_token        = ENV['OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
-  config.auth_method = :oauth
-end
-
-# db setup
-uri = URI.parse(ENV["BOXEN_REDIS_URL"] || ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 # my options
 VERBOSE = ENV["VERBOSE"] || false
