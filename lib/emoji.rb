c87e66351517b5fdca746176327ace793c90e6b0
Matthew Rothenberg <mrothenberg@gmail.com>
2013-05-24 21:24:08 -0400

    stubbing out a basic emoji char class

--- lib/emoji.rb
+++ lib/emoji.rb
@@ -25,6 +25,22 @@ class Emoji
   end
 
 end
+
+class EmojiChar
+  attr_reader :name, :unified, :short_name
+
+  def initialize(emoji_hash)
+    @name = emoji_hash['name']
+    @unified = emoji_hash['unified']
+    @docomo = emoji_hash['docomo']
+    @au = emoji_hash['au']
+    @softbank = emoji_hash['softbank']
+    @google = emoji_hash['google']
+    @image = emoji_hash['image']
+    @sheet_x = emoji_hash['sheet_x']
+    @sheet_y = emoji_hash['sheet_y']
+    @short_name = emoji_hash['short_name']
+    @text = emoji_hash['text']
   end
 
 end
