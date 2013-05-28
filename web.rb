7a63e2fb54634b903ab5b92d7d314804f5add976
Matthew Rothenberg <mrothenberg@gmail.com>
2013-05-28 11:47:46 -0400

    password protect for now

--- web.rb
+++ web.rb
@@ -5,9 +5,23 @@ require 'sinatra'
 require 'slim'
 require 'coffee-script'
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['swg', 'swg']
+  end
+end
+
 set :public_folder, 'public'
 
 get '/' do
+  protected!
   slim :index
 end
 
