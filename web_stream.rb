f54d36032380d1a8f930ab6fe5effde141a96cf6
Matthew Rothenberg <mrothenberg@gmail.com>
2013-06-25 16:13:03 -0400

    standardize connections variable name now that things are in classes
    modular ftw

--- web_stream.rb
+++ web_stream.rb
@@ -61,15 +61,15 @@ end
 # streaming thread for score updates (main page)
 ################################################
 class WebScoreRawStreamer < Sinatra::Base
-  set :raw_conns, []
+  set :connections, []
 
   get '/raw' do
     content_type 'text/event-stream'
     stream(:keep_open) do |out|
       out = WrappedStream.new(out, request)
       out.sse_set_retry(SSE_SCORE_RETRY_MS) if SSE_FORCE_REFRESH
-      settings.raw_conns << out
-      out.callback { settings.raw_conns.delete(out) }
+      settings.connections << out
+      out.callback { settings.connections.delete(out) }
       if SSE_FORCE_REFRESH then EM.add_timer(SSE_SCORE_FORCECLOSE_SEC) { out.close } end
     end
   end
@@ -78,7 +78,7 @@ class WebScoreRawStreamer < Sinatra::Base
     t_redis = Redis.new(:host => REDIS_URI.host, :port => REDIS_URI.port, :password => REDIS_URI.password, :driver => :hiredis)
     t_redis.psubscribe('stream.score_updates') do |on|
       on.pmessage do |match, channel, message|
-        raw_conns.each do |out|
+        connections.each do |out|
           out.sse_data(message)
         end
       end
@@ -92,7 +92,7 @@ end
 ################################################
 class WebScoreCachedStreamer < Sinatra::Base
 
-  set :eps_conns, []
+  set :connections, []
   cached_scores = {}
   semaphore = Mutex.new
 
@@ -101,11 +101,11 @@ class WebScoreCachedStreamer < Sinatra::Base
     stream(:keep_open) do |conn|
       conn = WrappedStream.new(conn, request)
       conn.sse_set_retry(SSE_SCORE_RETRY_MS) if SSE_FORCE_REFRESH
-      settings.eps_conns << conn
+      settings.connections << conn
       puts "STREAM: new eps_stream connection opened for #{request.ip}" if VERBOSE
       conn.callback do
         puts "STREAM: eps_stream connection closed for #{request.ip}" if VERBOSE
-        settings.eps_conns.delete(conn)
+        settings.connections.delete(conn)
       end
       if SSE_FORCE_REFRESH then EM.add_timer(SSE_SCORE_FORCECLOSE_SEC) { conn.close } end
     end
@@ -119,7 +119,7 @@ class WebScoreCachedStreamer < Sinatra::Base
         cached_scores.clear
       end
 
-      eps_conns.each do |out|
+      connections.each do |out|
         out.sse_data(Oj.dump scores) unless scores.empty?
       end
 
@@ -155,7 +155,7 @@ class WebDetailStreamer < Sinatra::Base
     end
   end
 
-  set :detail_conns, []
+  set :connections, []
   get '/details/:char' do
     content_type 'text/event-stream'
     stream(:keep_open) do |out|
@@ -166,7 +166,7 @@ class WebDetailStreamer < Sinatra::Base
       puts "STREAM: new detailstream connection for #{ts.tag} from #{request.ip}" if VERBOSE
       out.callback do
         puts "STREAM: detailstream connection closed for #{ts.tag} from #{request.ip}" if VERBOSE
-        settings.detail_conns.delete(ts)
+        settings.connections.delete(ts)
       end
       if SSE_FORCE_REFRESH then EM.add_timer(SSE_DETAIL_FORCECLOSE_SEC) { out.close } end
     end
@@ -177,7 +177,7 @@ class WebDetailStreamer < Sinatra::Base
     t_redis.psubscribe('stream.tweet_updates.*') do |on|
       on.pmessage do |match, channel, message|
         channel_id = channel.split('.')[2] #TODO: perf profile this versus a regex later
-        detail_conns.select { |c| c.tag == channel_id}.each do |ts|
+        connections.select { |c| c.tag == channel_id}.each do |ts|
           ts.out.sse_event_data(channel, message)
         end
       end
@@ -199,9 +199,9 @@ class WebStreamer < Sinatra::Base
   @stream_graphite_log_rate = 10
   EM.next_tick do
     EM::PeriodicTimer.new(@stream_graphite_log_rate) do
-      graphite_dyno_log("stream.raw.clients", WebScoreRawStreamer.raw_conns.count)
-      graphite_dyno_log("stream.eps.clients", WebScoreCachedStreamer.eps_conns.count)
-      graphite_dyno_log("stream.detail.clients", WebDetailStreamer.detail_conns.count)
+      graphite_dyno_log("stream.raw.clients", WebScoreRawStreamer.connections.count)
+      graphite_dyno_log("stream.eps.clients", WebScoreCachedStreamer.connections.count)
+      graphite_dyno_log("stream.detail.clients", WebDetailStreamer.connections.count)
     end
   end
 
